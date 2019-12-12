96dcafaa2d6494ebec47232d1da18700bca1ac7c
abdsmd <abdsmd@gmail.com>
2019-12-12 12:43:08 +0600

    Update checl-all-server.sh

--- checl-all-server.sh
+++ checl-all-server.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
-input="list.txt"
+list="list.txt"
+rm -rf $list
+touch $list
+chmod 777 $list
+speedtest-cli --list > $list
 while IFS= read -r line
 do
         N=$(echo $line | cut -d')' -f2-4 )
         A=$(echo $line | cut -d')' -f1 )
+if  [  -z "${A##[0-9]*}" ]
+then
         echo "Testing - $N [$A] : "
         speedtest-cli --server $A --simple
         echo ""
-done < "$input"
+fi
+done < "$list"
